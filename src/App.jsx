940bdec17b92d1f7049a7e5a103a5d12448a6a70
kgy-inst <bestyoona1@gmail.com>
2025-10-02 11:51:53 +0900

    fix: App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import Header from './components/common/Header'
-import Footer from './components/common/Footer'
-import ChatbotButton from './components/common/ChatbotButton'
-import './App.css'
+import CompleteButton from "./components/board/CompleteButton";
+import TalkDetail from "./components/board/TalkDetail";
 
 function App() {
-
   return (
-    
     <>
-      <Header /> 
-      <main style={{ minHeight: 'calc(100vh - 100px)', padding: '20px' }}>
-          {/* 임시 콘텐츠: 푸터를 아래로 밀어내기 위해 min-height를 설정했습니다. */}
-          <h1>웹페이지 콘텐츠 영역</h1>
-          <p>푸터가 하단에 고정되었는지 확인해주세요.</p>
-      </main>
-       
-      <Footer />
-      <ChatbotButton />
+      <div>
+        <TalkDetail />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
