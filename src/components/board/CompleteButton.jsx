b37bb8555277799bf2298b9eec39daf12886bef1
anjoo-k <knjou92@naver.com>
2025-10-02 10:46:53 +0900

    Merge remote-tracking branch 'origin/feat/soonhyeong-component' into feat/knj-components
    # Conflicts:
    #	src/index.css

---
+++ src/components/board/CompleteButton.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+import React from "react";
+
+const CompleteButton = () => {
+  return (
+    <div>
+      <button className="complete-btn">완료</button>
+    </div>
+  );
+};
+
+export default CompleteButton;
