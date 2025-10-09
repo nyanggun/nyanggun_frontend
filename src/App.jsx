32525d20a423dc20c8b74a5c96382640f147736f
soon-hyeong <shjoo820@naver.com>
2025-10-10 05:42:37 +0900

    feat : 시큐리티, 로그인/회원가입 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 
+import { AuthProvider } from "./contexts/AuthProvider";
 import ExplorationAndTalkPage from "./pages/board/ExplorationAndTalkPage";
 
 import BadgeAcquisition from "./pages/badge/BadgeAcquisition";
@@ -16,25 +17,31 @@ import TalkUpdate from "./pages/board/talk/TalkUpdate";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import ChatbotButton from "./components/common/chatbot/ChatbotButton";
+import LoginPage from "./pages/board/login-register/LoginPage";
+import RegisterPage from "./pages/board/login-register/RegisterPage";
 
 const App = () => {
 	return (
 		<>
-			<Header />
-			<Routes>
-				<Route path="/dorandoran" element={<ExplorationAndTalkPage />}>
-					<Route index element={<Navigate to="exploration" replace />} />
-					<Route path="exploration" element={<ExplorationBoard />}></Route>
-					<Route path="exploration/:id" element={<ExplorationDetailPage />} />
-					<Route path="exploration/:id/edit" element={<ExplorationEdit />} />
-					<Route path="exploration/new" element={<NewExploration></NewExploration>}></Route>
-					<Route path="talks" element={<TalkBoardList></TalkBoardList>}></Route>
-					<Route path="talks/detail/:talkId" element={<TalkBoardDetail></TalkBoardDetail>}></Route>
-					<Route path="talks/new" element={<TalkNew></TalkNew>}></Route>
-					<Route path="talks/update" element={<TalkUpdate></TalkUpdate>}></Route>
-				</Route>
-			</Routes>
-			<Footer />
+			<AuthProvider>
+				<Header />
+				<Routes>
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
+					<Route path="/dorandoran" element={<ExplorationAndTalkPage />}>
+						<Route index element={<Navigate to="exploration" replace />} />
+						<Route path="exploration" element={<ExplorationBoard />}></Route>
+						<Route path="exploration/:id" element={<ExplorationDetailPage />} />
+						<Route path="exploration/:id/edit" element={<ExplorationEdit />} />
+						<Route path="exploration/new" element={<NewExploration></NewExploration>}></Route>
+						<Route path="talks" element={<TalkBoardList></TalkBoardList>}></Route>
+						<Route path="talks/detail/:talkId" element={<TalkBoardDetail></TalkBoardDetail>}></Route>
+						<Route path="talks/new" element={<TalkNew></TalkNew>}></Route>
+						<Route path="talks/update" element={<TalkUpdate></TalkUpdate>}></Route>
+					</Route>
+				</Routes>
+				<Footer />
+			</AuthProvider>
 		</>
 	);
 };
