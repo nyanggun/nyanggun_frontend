76a2399005695d7e6180524706235f0f164334d5
khgeung <khgeung5357@gmail.com>
2025-10-13 23:34:22 +0900

    feat : 검색 결과 페이지 생성

--- src/pages/board/ExplorationAndTalkPage.jsx
+++ src/pages/board/ExplorationAndTalkPage.jsx
@@ -1,6 +1,6 @@
 //탐방기와 담소 게시판 입니다.
 import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../../components/common/menu/Menu";
 import "./ExplorationAndTalkPage.css";
 import Subtitle from "../../components/board/Subtitle";
@@ -11,50 +11,56 @@ import ExplorationBoard from "./Exploration/ExplorationBoard";
 import WritePostButton from "../../components/board/WritePostButton";
 
 const ExplorationAndTalkPage = () => {
-	// 처음 페이지에서는 탐방기부터 보여줍니다. -> 주석처리 : ReactRouterDom의 <Outlet>을 사용하여 하위 URL로 표현
-	// const [menuState, setMenuState] = useState({
-	// 	isMenuOneClick: true,
-	// 	isMenuTwoClick: false,
-	// });
-	// const [selectedTalkId, setSelectedTalkId] = useState(null);
-	// const handleMenuChange = (newState) => {
-	// 	setMenuState(newState);
-	// 	setSelectedTalkId(null);
-	// };
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [searchKeyword, setSearchKeyword] = useState("");
+    const isPostListPage =
+        location.pathname === "/dorandoran/explorations" ||
+        location.pathname === "/dorandoran/talks";
 
-	// const writePostButtonProps =
-	// const useEffect(()=>{
+    //검색 함수 입니다.
+    const handleSearch = (keyword) => {
+        if (location.pathname.startsWith("/dorandoran/explorations")) {
+            return;
+        } else if (location.pathname.startsWith("/dorandoran/talks")) {
+            //console.log("도란도란의 토크 페이지로 키워드 전달", keyword);
+            setSearchKeyword(keyword);
+            navigate(
+                `/dorandoran/talks/search?keyword=${encodeURIComponent(
+                    keyword
+                )}`
+            );
+        }
+    };
+    return (
+        <div>
+            <div className="board-page">
+                <div className="search-margin">
+                    <Subtitle
+                        text={"도란도란"}
+                        onSearchBoard={handleSearch}
+                    ></Subtitle>
+                </div>
+            </div>
 
-	// }, [isMenuOneClick])
+            <div className="menu-margin">
+                <Menu
+                    menuOne={"문화재 탐방기"}
+                    menuOneLink={"/dorandoran/explorations"}
+                    menuTwoLink={"/dorandoran/talks"}
+                    menuTwo={"문화재 담소"} /*chooseMenu={handleMenuChange}*/
+                ></Menu>
+            </div>
+            {/* outlet을 적용한 코드 */}
+            <main>
+                <Outlet context={{ searchKeyword }} />
+            </main>
 
-	const location = useLocation();
-	const isPostListPage =
-		location.pathname === "/dorandoran/explorations" || location.pathname === "/dorandoran/talks";
-
-	return (
-		<div>
-			<div className="board-page">
-				<div className="search-margin">
-					<Subtitle text={"도란도란"}></Subtitle>
-				</div>
-			</div>
-
-			<div className="menu-margin">
-				<Menu
-					menuOne={"문화재 탐방기"}
-					menuOneLink={"/dorandoran/explorations"}
-					menuTwoLink={"/dorandoran/talks"}
-					menuTwo={"문화재 담소"} /*chooseMenu={handleMenuChange}*/
-				></Menu>
-			</div>
-			{/* outlet을 적용한 코드 */}
-			<main>
-				<Outlet />
-			</main>
-
-			{isPostListPage && <WritePostButton location={window.location.href} />}
-		</div>
-	);
+            {isPostListPage && (
+                <WritePostButton location={window.location.href} />
+            )}
+        </div>
+    );
 };
 
 export default ExplorationAndTalkPage;
