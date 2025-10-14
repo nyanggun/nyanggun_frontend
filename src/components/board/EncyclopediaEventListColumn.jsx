fa022e35f1df4b16cc68c0ed582fbe4aee094e4f
anjoo-k <knjou92@naver.com>
2025-10-14 14:29:02 +0900

    feat : 문화재도감 리스트, 상세 페이지 구현

--- src/components/board/EncyclopediaEventListColumn.jsx
+++ src/components/board/EncyclopediaEventListColumn.jsx
@@ -1,59 +1,48 @@
 import React from "react";
-import { Col, Container, Row, Image } from "react-bootstrap";
+import { Col, Row, Image } from "react-bootstrap";
 import BookmarkButton from "./BookmarkButton";
 import MemberButton from "./MemberButton";
 
-const EncyclopediaEventListColumn = (props) => {
-	return (
-		<Row className="column align-items-center m-0 pb-1 h-25">
-			<Col className="d-flex justify-content-center align-items-center m-0 p-0 h-100 bg-dark" xs={6}>
-				<Image src={props.image} fluid></Image>
-			</Col>
-			<Col className="bg-white h-100 d-flex flex-column" xs={6}>
-				<Row className="my-1">
-					<Col>
-						<span className="fs-5">{props.title}</span>
-					</Col>
-				</Row>
-				{/*  */}
-				<Row className="my-1">
-					<Col>
-						<span className="fs-6">
-							{props.description.length > 20 ? props.description.slice(0, 26) + "..." : props.description}
-						</span>
-					</Col>
-				</Row>
-				{props.age != null && (
-					<Row className="my-1">
-						<Col>
-							<span className="fs-6">{props.age}</span>
-						</Col>
-					</Row>
-				)}
-				{props.date != null && (
-					<Row className="my-1">
-						<Col>
-							<span className="fs-6">{props.date}</span>
-						</Col>
-					</Row>
-				)}
-				<Row className="my-1">
-					<Col>
-						<span className="fs-6">{props.location}</span>
-					</Col>
-				</Row>
-				{/* -- 하단 버튼 컨텐츠 -- */}
-				{/* 2. 버튼들을 담을 Row에 mt-auto를 적용해 맨 아래로 밀어냅니다. */}
-				<Row className="mt-auto">
-					<Col className="d-flex justify-content-end align-items-center pe-1">
-						{/* 두 버튼이 함께 있도록 같은 Col 안에 배치합니다. */}
-						{props.bookmark != null && <BookmarkButton count={props.bookmark} />}
-						{props.member != null && <MemberButton count={props.member} />}
-					</Col>
-				</Row>
-			</Col>
-		</Row>
-	);
+const EncyclopediaEventListColumn = ({ heritage }) => {
+  return (
+    <Row className="g-0 pb-1 h-25 mt-4 justify-content-center">
+      <Col className="m-0 p-0" xs={12} sm={10} md={8} lg={6}>
+        <Image src={heritage.imageUrl} fluid></Image>
+      </Col>
+      <Col className="m-0 p-1" xs={12} sm={10} md={8} lg={6}>
+        <div>
+          <div className="mb-3">
+            <span className="fs-4">
+              <strong>{heritage.name}</strong>
+            </span>
+          </div>
+          <div className="mb-4">
+            <span className="fs-5">
+              {heritage?.content?.length > 0
+                ? heritage.content.slice(0, 70) + "..."
+                : heritage.content}
+            </span>
+          </div>
+
+          {heritage.period != null && (
+            <div className="mb-1">
+              <span className="fs-6">{heritage.period}</span>
+            </div>
+          )}
+          <div>
+            <span className="fs-6">{heritage.address}</span>
+          </div>
+        </div>
+        {/* -- 하단 버튼 컨텐츠 -- */}
+        {/* 2. 버튼들을 담을 Row에 mt-auto를 적용해 맨 아래로 밀어냅니다. */}
+        <Row className="mt-auto">
+          <Col className="d-flex justify-content-end align-items-center pe-1">
+            {heritage.bookmark && <BookmarkButton count={heritage.bookmark} />}
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 };
 
 export default EncyclopediaEventListColumn;
