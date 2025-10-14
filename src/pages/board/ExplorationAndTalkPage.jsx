d97159cd3162a24fc78eae9667095d0d3c8080e9
khgeung <khgeung5357@gmail.com>
2025-10-14 17:48:20 +0900

    feat : 사진함 무한 스크롤 구현

--- src/pages/board/ExplorationAndTalkPage.jsx
+++ src/pages/board/ExplorationAndTalkPage.jsx
@@ -11,56 +11,49 @@ import ExplorationBoard from "./Exploration/ExplorationBoard";
 import WritePostButton from "../../components/board/WritePostButton";
 
 const ExplorationAndTalkPage = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const [searchKeyword, setSearchKeyword] = useState("");
-    const isPostListPage =
-        location.pathname === "/dorandoran/explorations" ||
-        location.pathname === "/dorandoran/talks";
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const isPostListPage =
+    location.pathname === "/dorandoran/explorations" ||
+    location.pathname === "/dorandoran/talks";
 
-    //검색 함수 입니다.
-    const handleSearch = (keyword) => {
-        if (location.pathname.startsWith("/dorandoran/explorations")) {
-            return;
-        } else if (location.pathname.startsWith("/dorandoran/talks")) {
-            //console.log("도란도란의 토크 페이지로 키워드 전달", keyword);
-            setSearchKeyword(keyword);
-            navigate(
-                `/dorandoran/talks/search?keyword=${encodeURIComponent(
-                    keyword
-                )}`
-            );
-        }
-    };
-    return (
-        <div>
-            <div className="board-page">
-                <div className="search-margin">
-                    <Subtitle
-                        text={"도란도란"}
-                        onSearchBoard={handleSearch}
-                    ></Subtitle>
-                </div>
-            </div>
-
-            <div className="menu-margin">
-                <Menu
-                    menuOne={"문화재 탐방기"}
-                    menuOneLink={"/dorandoran/explorations"}
-                    menuTwoLink={"/dorandoran/talks"}
-                    menuTwo={"문화재 담소"} /*chooseMenu={handleMenuChange}*/
-                ></Menu>
-            </div>
-            {/* outlet을 적용한 코드 */}
-            <main>
-                <Outlet context={{ searchKeyword }} />
-            </main>
-
-            {isPostListPage && (
-                <WritePostButton location={window.location.href} />
-            )}
+  //검색 함수 입니다.
+  const handleSearch = (keyword) => {
+    if (location.pathname.startsWith("/dorandoran/explorations")) {
+      return;
+    } else if (location.pathname.startsWith("/dorandoran/talks")) {
+      //console.log("도란도란의 토크 페이지로 키워드 전달", keyword);
+      setSearchKeyword(keyword);
+      navigate(
+        `/dorandoran/talks/search?keyword=${encodeURIComponent(keyword)}`
+      );
+    }
+  };
+  return (
+    <div>
+      <div className="board-page">
+        <div className="search-margin">
+          <Subtitle text={"도란도란"} onSearchBoard={handleSearch}></Subtitle>
         </div>
-    );
+      </div>
+
+      <div className="menu-margin">
+        <Menu
+          menuOne={"문화재 탐방기"}
+          menuOneLink={"/dorandoran/explorations"}
+          menuTwoLink={"/dorandoran/talks"}
+          menuTwo={"문화재 담소"} /*chooseMenu={handleMenuChange}*/
+        ></Menu>
+      </div>
+      {/* outlet을 적용한 코드 */}
+      <main>
+        <Outlet />
+      </main>
+
+      {isPostListPage && <WritePostButton location={window.location.href} />}
+    </div>
+  );
 };
 
 export default ExplorationAndTalkPage;
