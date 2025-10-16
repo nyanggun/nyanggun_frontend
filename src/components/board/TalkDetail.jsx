9b667092f71191f1aa5dbd043ab1af7d0c3e385e
khgeung <khgeung5357@gmail.com>
2025-10-16 23:58:55 +0900

    feat : 담소 사진 업로드 구현

--- src/components/board/TalkDetail.jsx
+++ src/components/board/TalkDetail.jsx
@@ -3,173 +3,226 @@ import "./TalkDetail.css";
 import GeoAltIcon from "../../assets/geo-alt-icon.svg";
 import ReportIcon from "../../assets/report-icon.svg";
 import TextIcon from "../../assets/text-icon.svg";
-import { Card, Container, Row, Col, Image } from "react-bootstrap";
+import { Card, Container, Row, Col, Image, Carousel } from "react-bootstrap";
 import BorderButton from "./BorderButton";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
 const TalkDetail = ({
-  talk,
-  onClick,
-  onDeleteTalk,
-  onUpdateTalk,
-  onBookmark,
+    talk,
+    onClick,
+    onDeleteTalk,
+    onUpdateTalk,
+    onBookmark,
 }) => {
-  const userData = useContext(AuthContext);
-  //시간 데이터(LocalDateTime)을 변환하여 1분 전 <-과 같은 형식으로 만들기
-  //서버에서는 2025-10-02T15:32:00 로 받아올 때 사용 가능하다.
-  const [timeAgo, setTimeAgo] = useState("");
-  const currentPath = location.pathname;
-  const formatTimeAgo = (time) => {
-    const now = new Date();
-    const past = new Date(time); // 서버에서 받은 LocalDateTime 문자열
-    const diff = Math.floor((now - past) / 1000); // 초 단위 차이
+    const userData = useContext(AuthContext);
+    //시간 데이터(LocalDateTime)을 변환하여 1분 전 <-과 같은 형식으로 만들기
+    //서버에서는 2025-10-02T15:32:00 로 받아올 때 사용 가능하다.
+    const [timeAgo, setTimeAgo] = useState("");
+    const currentPath = location.pathname;
+    const formatTimeAgo = (time) => {
+        const now = new Date();
+        const past = new Date(time); // 서버에서 받은 LocalDateTime 문자열
+        const diff = Math.floor((now - past) / 1000); // 초 단위 차이
 
-    if (diff < 60) return "방금 전";
-    if (diff < 3600) return `${Math.floor(diff / 60)}분 전`;
-    if (diff < 86400) return `${Math.floor(diff / 3600)}시간 전`;
-    return `${Math.floor(diff / 86400)}일 전`;
-  };
-  useEffect(() => {
-    // 처음 렌더링 시 계산
-    setTimeAgo(formatTimeAgo(talk.createdAt));
+        if (diff < 60) return "방금 전";
+        if (diff < 3600) return `${Math.floor(diff / 60)}분 전`;
+        if (diff < 86400) return `${Math.floor(diff / 3600)}시간 전`;
+        return `${Math.floor(diff / 86400)}일 전`;
+    };
+    useEffect(() => {
+        // 처음 렌더링 시 계산
+        setTimeAgo(formatTimeAgo(talk.createdAt));
 
-    // 초 단위로 업데이트
-    const interval = setInterval(() => {
-      setTimeAgo(formatTimeAgo(talk.createdAt));
-    }, 1000); // 1초마다 갱신
+        // 초 단위로 업데이트
+        const interval = setInterval(() => {
+            setTimeAgo(formatTimeAgo(talk.createdAt));
+        }, 1000); // 1초마다 갱신
 
-    return () => clearInterval(interval);
-  }, [talk.createdAt]);
+        return () => clearInterval(interval);
+    }, [talk.createdAt]);
 
-  return (
-    <Row
-      onClick={onClick}
-      className="h-100 justify-content-center align-items-center m-0"
-    >
-      <Col xs={12} sm={10} md={8} lg={6}>
-        <Card className="border-0">
-          <Card.Body className="pt-0 pb-0 px-3">
-            <Card.Title>{talk.title}</Card.Title>
-            <div className="d-flex align-items-center gap-2">
-              {talk.profile ? (
-                <div>프로필 사진이 있는 경우 이 부분을 고치세요.</div>
-              ) : (
-                <div className="talk-profile">
-                  <Image
-                    src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
-                    roundedCircle
-                    fluid
-                    className="talk-profile-pic border border-1"
-                  />
-                </div>
-              )}
-              <div>
-                <span>{talk.nickname}</span>
-              </div>
-              <div>
-                <span className="small">{timeAgo}</span>
-              </div>
-            </div>
-            {talk.talkPictureList ? (
-              <div>
-                <img
-                  className="talk-photo-imgsize"
-                  src={`http://localhost:8080${talk.talkPictureList.path}`}
-                ></img>
-              </div>
-            ) : (
-              <div></div>
-            )}
-            {talk.tags && talk.tags.length > 0 ? (
-              <div className="tags">
-                {talk.tags.map((tag, index) => (
-                  <span key={index}>{tag} </span>
-                ))}
-              </div>
-            ) : (
-              <div></div>
-            )}
+    return (
+        <Row
+            onClick={onClick}
+            className="h-100 justify-content-center align-items-center m-0"
+        >
+            <Col xs={12} sm={10} md={8} lg={6}>
+                <Card className="border-0">
+                    <Card.Body className="pt-0 pb-0 px-3">
+                        <Card.Title>{talk.title}</Card.Title>
+                        <div className="d-flex align-items-center gap-2">
+                            {talk.profile ? (
+                                <div>
+                                    프로필 사진이 있는 경우 이 부분을 고치세요.
+                                </div>
+                            ) : (
+                                <div className="talk-profile">
+                                    <Image
+                                        src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+                                        roundedCircle
+                                        fluid
+                                        className="talk-profile-pic border border-1"
+                                    />
+                                </div>
+                            )}
+                            <div>
+                                <span>{talk.nickname}</span>
+                            </div>
+                            <div>
+                                <span className="small">{timeAgo}</span>
+                            </div>
+                        </div>
+                        {talk.photoBoxPicturePath ? (
+                            <div>
+                                <img
+                                    className="talk-photo-imgsize-photobox"
+                                    src={`http://localhost:8080${talk.photoBoxPicturePath}`}
+                                ></img>
+                            </div>
+                        ) : (
+                            <div></div>
+                        )}
 
-            <Card.Text
-              className={`mt-3 ${
-                currentPath.startsWith("/dorandoran/talks/detail") ||
-                currentPath.startsWith("/photobox/detail")
-                  ? ""
-                  : "multi-line-ellipsis"
-              }`}
-            >
-              {talk.content}
-            </Card.Text>
-            <div className="card-setting align-items-center gap-1">
-              <div className="card-box">
-                <div className="me-1">
-                  <BookmarkButton
-                    count={talk.bookmarkCount}
-                    onBookmark={onBookmark}
-                    isBookmarked={talk.bookmarked}
-                  ></BookmarkButton>
-                </div>
-                {/* 사진함은 댓글이 없으므로 해당 아이콘을 보이지 않게 합니다. */}
-                {talk.commentCount > -1 ? (
-                  <div className="icon-border d-flex align-items-center gap-1 pt-1 ps-2 pe-2 pb-1 me-1">
-                    <Image fluid className="icons" src={TextIcon} width="15" />
-                    <span className="small">{talk.commentCount}</span>
-                  </div>
-                ) : (
-                  <span></span>
-                )}
-                {/* 담소는 연관 문화재가 없으므로 해당 아이콘을 안보이게 합니다. */}
-                {talk.relatedHeritage ? (
-                  <div className="icon-border d-flex align-items-center gap-1 p-1 me-1">
-                    <Image fluid className="icons" src={GeoAltIcon} />
-                    <span className="small">{talk.relatedHeritage}</span>
-                  </div>
-                ) : (
-                  <div></div>
-                )}
+                        {talk.talkPictureList &&
+                        talk.talkPictureList.length > 0 ? (
+                            <Carousel
+                                interval={null}
+                                className="mt-3"
+                                // onClick={(e) => e.stopPropagation()} // 상위 클릭 이벤트 막기
+                            >
+                                {talk.talkPictureList.map((picture, index) => (
+                                    <Carousel.Item
+                                        key={index}
+                                        style={{
+                                            width: "100%",
+                                            height: "270px",
+                                            overflow: "hidden",
+                                            backgroundColor: "black",
+                                            borderRadius: "20px",
+                                        }}
+                                    >
+                                        <div className="talk-photo-imgsize">
+                                            <img
+                                                style={{
+                                                    maxHeight: "450px",
+                                                    objectFit: "contain", // 이미지가 잘리지 않게 비율 유지
+                                                    margin: "0 auto",
+                                                }}
+                                                className="talk-photo-imgsize"
+                                                src={`http://localhost:8080${picture.path}`}
+                                            ></img>
+                                        </div>
+                                    </Carousel.Item>
+                                ))}
+                            </Carousel>
+                        ) : (
+                            <div></div>
+                        )}
+                        {talk.tags && talk.tags.length > 0 ? (
+                            <div className="tags">
+                                {talk.tags.map((tag, index) => (
+                                    <span key={index}>{tag} </span>
+                                ))}
+                            </div>
+                        ) : (
+                            <div></div>
+                        )}
 
-                <div className="talk-detail-report">
-                  <Image
-                    fluid
-                    className="icons d-flex align-items-center"
-                    src={ReportIcon}
-                  />
-                </div>
-              </div>
-              {/* 게시글 작성 아이디 = 유저 번호 */}
+                        <Card.Text
+                            className={`mt-3 ${
+                                currentPath.startsWith(
+                                    "/dorandoran/talks/detail"
+                                ) || currentPath.startsWith("/photobox/detail")
+                                    ? ""
+                                    : "multi-line-ellipsis"
+                            }`}
+                        >
+                            {talk.content}
+                        </Card.Text>
+                        <div className="card-setting align-items-center gap-1">
+                            <div className="card-box">
+                                <div className="me-1">
+                                    <BookmarkButton
+                                        count={talk.bookmarkCount}
+                                        onBookmark={onBookmark}
+                                        isBookmarked={talk.bookmarked}
+                                    ></BookmarkButton>
+                                </div>
+                                {/* 사진함은 댓글이 없으므로 해당 아이콘을 보이지 않게 합니다. */}
+                                {talk.commentCount > -1 ? (
+                                    <div className="icon-border d-flex align-items-center gap-1 pt-1 ps-2 pe-2 pb-1 me-1">
+                                        <Image
+                                            fluid
+                                            className="icons"
+                                            src={TextIcon}
+                                            width="15"
+                                        />
+                                        <span className="small">
+                                            {talk.commentCount}
+                                        </span>
+                                    </div>
+                                ) : (
+                                    <span></span>
+                                )}
+                                {/* 담소는 연관 문화재가 없으므로 해당 아이콘을 안보이게 합니다. */}
+                                {talk.relatedHeritage ? (
+                                    <div className="icon-border d-flex align-items-center gap-1 p-1 me-1">
+                                        <Image
+                                            fluid
+                                            className="icons"
+                                            src={GeoAltIcon}
+                                        />
+                                        <span className="small">
+                                            {talk.relatedHeritage}
+                                        </span>
+                                    </div>
+                                ) : (
+                                    <div></div>
+                                )}
 
-              {/* 현재 페이지가 상세 페이지일때만 버튼 노출 */}
-              {currentPath.startsWith("/dorandoran/talks/detail") ||
-              currentPath.startsWith("/photobox/detail") ? (
-                talk.memberId === userData.user?.id ? (
-                  <div className="comment-btn-delete">
-                    <BorderButton
-                      btnName="수정"
-                      buttonColor="black"
-                      clickBtn={onUpdateTalk}
-                    />
-                    <span> </span>
-                    <BorderButton
-                      btnName="삭제"
-                      buttonColor="red"
-                      clickBtn={onDeleteTalk}
-                    />
-                  </div>
-                ) : (
-                  <div></div>
-                )
-              ) : (
-                <div></div>
-              )}
-            </div>
+                                <div className="talk-detail-report">
+                                    <Image
+                                        fluid
+                                        className="icons d-flex align-items-center"
+                                        src={ReportIcon}
+                                    />
+                                </div>
+                            </div>
+                            {/* 게시글 작성 아이디 = 유저 번호 */}
 
-            <hr className="border-1 p-0"></hr>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  );
+                            {/* 현재 페이지가 상세 페이지일때만 버튼 노출 */}
+                            {currentPath.startsWith(
+                                "/dorandoran/talks/detail"
+                            ) || currentPath.startsWith("/photobox/detail") ? (
+                                talk.memberId === userData.user?.id ? (
+                                    <div className="comment-btn-delete">
+                                        <BorderButton
+                                            btnName="수정"
+                                            buttonColor="black"
+                                            clickBtn={onUpdateTalk}
+                                        />
+                                        <span> </span>
+                                        <BorderButton
+                                            btnName="삭제"
+                                            buttonColor="red"
+                                            clickBtn={onDeleteTalk}
+                                        />
+                                    </div>
+                                ) : (
+                                    <div></div>
+                                )
+                            ) : (
+                                <div></div>
+                            )}
+                        </div>
+
+                        <hr className="border-1 p-0"></hr>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    );
 };
 
 export default TalkDetail;
--- src/pages/board/talk/TalkUpdate.jsx
+++ src/pages/board/talk/TalkUpdate.jsx
@@ -1,92 +1,183 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import WritePostInputBox from "../../../components/board/WritePostInputBox";
 import WritingEditor from "../../../components/board/WritingEditor";
 import api from "../../../config/apiConfig";
 import "./TalkNew.css";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import BorderButton from "../../../components/board/BorderButton";
 import { AuthContext } from "../../../contexts/AuthContext";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 
 //담소 게시글을 수정할 수 있는 페이지 입니다.
 const TalkUpdate = () => {
-  const location = useLocation();
-  const userData = useContext(AuthContext);
-  const {
-    talkId,
-    title: initialTitle,
-    content: initialContent,
-  } = location.state || {};
-  const navigate = useNavigate();
-  const [title, setTitle] = useState(initialTitle || "");
-  const [contentOrigin, setContentOrigin] = useState(initialContent || "");
-  const content = contentOrigin.replace(/<[^>]+>/g, "");
+    const location = useLocation();
+    const userData = useContext(AuthContext);
+    const {
+        talkId,
+        title: initialTitle,
+        content: initialContent,
+    } = location.state || {};
+    const navigate = useNavigate();
+    const [title, setTitle] = useState(initialTitle || "");
+    const [contentOrigin, setContentOrigin] = useState(initialContent || "");
+    const content = contentOrigin.replace(/<[^>]+>/g, "");
 
-  //담소 게시글을 수정하는 메소드 입니다.
-  //서버로 요청을 보냅니다.
+    const [uploadImg, setUploadImg] = useState([]); // 배열로 관리
 
-  const handleTalkUpdate = async (title, content) => {
-    // 유효성 검사
-    if (!title.trim() || !content.trim()) {
-      alert("모든 내용을 입력해주세요");
-      return;
-    }
-    try {
-      await api.put(`/talks/${talkId}`, {
-        memberId: userData.user.id,
+    const fileInputRef = useRef(null);
+    const [uploadFiles, setUploadFiles] = useState(null);
+    const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
+
+    const talkData = {
         title,
         content,
-      });
-      console.log("담소 게시글을 수정했습니다.");
-      //서버에서 응답으로 해당 게시글의 id를 전달해줍니다.
-      //해당 id를 이용하여 상세 페이지로 이동합니다.
-      alert("게시글 수정이 완료되었습니다.");
-      navigate(`/dorandoran/talks/detail/${talkId}`);
-    } catch (error) {
-      console.log(
-        "담소 게시글을 수정하는 데 문제가 생겼습니다.",
-        error.message
-      );
-    }
-  };
+    };
 
-  return (
-    <Row className="row p-4 justify-content-center m-0  ">
-      <Col xs={12} sm={10} md={6} className=" m-0 p-0">
-        <div className="talk-new-container">
-          <WritePostInputBox
-            type={"text"}
-            placeholder={"제목"}
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          ></WritePostInputBox>
-          <WritingEditor
-            value={contentOrigin}
-            onChange={setContentOrigin}
-          ></WritingEditor>
+    //담소 게시글을 작성하는 메소드 입니다.
+    //서버로 요청을 보냅니다.
+    const handleTalkUpdate = async (title, content) => {
+        // 유효성 검사
+        if (!title.trim() || !content.trim()) {
+            alert("모든 내용을 입력해주세요");
+            return;
+        }
+        // FormData 생성
+        const formData = new FormData();
+        formData.append(
+            "talkData",
+            new Blob([JSON.stringify(talkData)], { type: "application/json" })
+        );
+        // 파일 여러 개 추가
+        if (uploadFiles && uploadFiles.length > 0) {
+            uploadFiles.forEach((file) => formData.append("files", file));
+            // 서버에서 "files"를 배열로 받도록 해야 함
+        }
+        try {
+            const response = await api.post("/talks", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            alert("게시글을 작성했습니다.");
+            console.log("담소 게시글을 작성했습니다.", response.data);
+            navigate("/dorandoran/talks");
+        } catch (error) {
+            console.error("담소 게시글을 작성하는 데 문제 발생", error.message);
+        }
+    };
 
-          <div className="talk-new-button">
-            <div className="talk-new-button-gap">
-              <BorderButton
-                btnName={"취소"}
-                buttonColor={"red"}
-                clickBtn={() => {
-                  navigate(`/dorandoran/talks`);
-                }}
-              ></BorderButton>
-            </div>
-            <div className="talk-new-button-gap">
-              <BorderButton
-                btnName={"완료"}
-                buttonColor={"black"}
-                clickBtn={() => handleTalkUpdate(title, content)}
-              ></BorderButton>
-            </div>
-          </div>
-        </div>
-      </Col>
-    </Row>
-  );
+    // 파일 선택 시 이미지 미리보기
+    const onchangeImageUpload = (e) => {
+        const files = Array.from(e.target.files);
+
+        // 이미 업로드된 이미지 + 새로 선택한 이미지 수가 4개 초과면 경고
+        if (uploadImg.length + files.length > 4) {
+            alert("사진은 4개까지 첨부가 가능합니다.");
+            e.target.value = "";
+            return;
+        }
+        const newImage = [];
+
+        for (let file of files) {
+            // 파일 크기 확인
+            if (file.size > MAX_FILE_SIZE) {
+                alert(
+                    "파일 크기가 너무 큽니다. 50MB 이하의 파일만 업로드할 수 있습니다."
+                );
+                e.target.value = ""; // 파일 input 초기화
+                continue;
+            }
+            newImage.push(URL.createObjectURL(file));
+        }
+        //하나라도 파일이 들어가면
+        if (newImage.length > 0) {
+            setUploadImg((prev) => [...prev, ...newImage].slice(0, 4));
+            setUploadFiles((prev) => [...(prev || []), ...files].slice(0, 4));
+            console.log("파일 하나 들어감");
+        }
+
+        e.target.value = "";
+    };
+
+    const handleImgUpload = () => {
+        fileInputRef.current.click();
+    };
+
+    //업로드한 사진 삭제
+    const handleRemoveImage = (index) => {
+        setUploadImg((prev) => prev.filter((_, i) => i !== index));
+        setUploadFiles((prev) => prev.filter((_, i) => i !== index));
+    };
+
+    return (
+        <Row className="row p-4 justify-content-center m-0  ">
+            <Col xs={12} sm={10} md={6} className=" m-0 p-0">
+                <div className="talk-new-container">
+                    <WritePostInputBox
+                        type={"text"}
+                        placeholder={"제목"}
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    ></WritePostInputBox>
+
+                    <WritingEditor
+                        value={contentOrigin}
+                        onChange={setContentOrigin}
+                    ></WritingEditor>
+                    <div className="mt-2">
+                        <Button
+                            type="button"
+                            className="photo-button"
+                            onClick={handleImgUpload}
+                        >
+                            이미지 업로드
+                        </Button>
+                    </div>
+                    <div>
+                        <input
+                            ref={fileInputRef}
+                            type="file"
+                            accept="image/*"
+                            onChange={onchangeImageUpload}
+                            style={{ display: "none" }}
+                        />
+                        <div className="talk-new-photo-img-container mt-2">
+                            {uploadImg &&
+                                uploadImg.map((uploadImg, index) => (
+                                    <img
+                                        className="talk-new-photo-imgsize"
+                                        key={index}
+                                        src={uploadImg}
+                                        alt="업로드된 이미지"
+                                        onClick={() => handleRemoveImage(index)}
+                                    />
+                                ))}
+                        </div>
+                    </div>
+                    <div className="talk-new-button">
+                        <div className="talk-new-button-gap">
+                            <BorderButton
+                                btnName={"취소"}
+                                buttonColor={"red"}
+                                clickBtn={() => {
+                                    navigate(`/dorandoran/talks`);
+                                }}
+                            ></BorderButton>
+                        </div>
+                        <div className="talk-new-button-gap">
+                            <BorderButton
+                                btnName={"완료"}
+                                buttonColor={"black"}
+                                clickBtn={() =>
+                                    handleTalkUpdate(title, content)
+                                }
+                            ></BorderButton>
+                        </div>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+    );
 };
 
 export default TalkUpdate;
