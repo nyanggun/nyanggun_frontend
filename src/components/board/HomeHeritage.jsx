d9b9ced4102db1fedd65df017ff2b48ac7a96c5b
khgeung <khgeung5357@gmail.com>
2025-10-18 00:18:49 +0900

    feat : 메인 페이지 퍼블리싱

--- src/components/board/HomeHeritage.jsx
+++ src/components/board/HomeHeritage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import main from "../../assets/images/1.png";
+import "./HomeHeritage.css";
 
 //홈에 보이는 문화재 도감 입니다.
 const HomeHeritage = () => {
-  return (
-    <div>
-      {/* <img src={main}></img> */}
-      <div>문화재 목록 출력하기</div>
-    </div>
-  );
+    return (
+        <div className="home-heritage-container">
+            <img src={main} className="home-heritage-img"></img>
+            <div>
+                <h4>문화재 이름</h4>
+                <h6>
+                    문화재 설명을 적습니다.문화재 설명을 적습니다.문화재 설명을
+                    적습니다.문화재 설명을 적습니다.문화재 설명을
+                    적습니다.문화재 설명을 적습니다.
+                </h6>
+            </div>
+        </div>
+    );
 };
 
 export default HomeHeritage;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,41 +5,49 @@ import "./Home.css";
 import HomeHeritage from "../../components/board/HomeHeritage";
 
 const Home = () => {
-  return (
-    <div className="home-main">
-      <div className="home-event">
-        <img src={main} className="home-image"></img>
-        <div className="home-event-title">
-          <h1>경복궁 동물 사냥 이벤트</h1>
-          <h4>
-            5종의 동물을 모두 찾아<br></br>
-            경복궁 동물 증표를 모아보세요!
-          </h4>
-          <h6>2025. 09. 23 ~ 2025. 10. 24</h6>
-        </div>
-        <div>
-          <HomeHeritage></HomeHeritage>
-          <HomeHeritage></HomeHeritage>
-          <HomeHeritage></HomeHeritage>
-          <HomeHeritage></HomeHeritage>
-        </div>
-      </div>
+    return (
+        <div className="home-main">
+            <div className="home-event">
+                <div>
+                    <img src={main} className="home-img"></img>
 
-      <div className="home-event">
-        <img src={photo} className="home-image"></img>
-        <div className="home-event-title">
-          <h1>금동미륵보살반가사유상</h1>
-          <h4>#국보 #반가사유상 #미륵불</h4>
+                    <div className="home-event-title">
+                        <h1>경복궁 동물 사냥 이벤트</h1>
+                        <h4>
+                            5종의 동물을 모두 찾아<br></br>
+                            경복궁 동물 증표를 모아보세요!
+                        </h4>
+                        <h6>2025. 09. 23 ~ 2025. 10. 24</h6>
+                    </div>
+                </div>
+                <div className="home-heritage">
+                    <HomeHeritage></HomeHeritage>
+                    <div className="home-heritage-margin">
+                        <HomeHeritage></HomeHeritage>
+                    </div>
+
+                    <HomeHeritage></HomeHeritage>
+                    <div className="home-heritage-margin">
+                        <HomeHeritage></HomeHeritage>
+                    </div>
+                </div>
+            </div>
+
+            <div className="home-event">
+                <img src={photo} className="home-image"></img>
+                <div className="home-event-title">
+                    <h1>금동미륵보살반가사유상</h1>
+                    <h4>#국보 #반가사유상 #미륵불</h4>
+                </div>
+                <div>
+                    <div>도감1</div>
+                    <div>도감1</div>
+                    <div>도감1</div>
+                    <div>도감1</div>
+                </div>
+            </div>
         </div>
-        <div>
-          <div>도감1</div>
-          <div>도감1</div>
-          <div>도감1</div>
-          <div>도감1</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
