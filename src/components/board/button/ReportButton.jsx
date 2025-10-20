a0ffb38f480b8db3cd4f5565e6f171552cf5232e
khgeung <khgeung5357@gmail.com>
2025-10-20 10:52:10 +0900

    feat : 담소, 사진함 신고 구현

--- src/components/board/button/ReportButton.jsx
+++ src/components/board/button/ReportButton.jsx
@@ -4,67 +4,77 @@ import { Button, Image } from "react-bootstrap";
 import "./ReportButton.css";
 import ReportImage from "../../../assets/report-icon.svg";
 
-const ReportModal = ({ onClose, reportPost, reportedPostId, reportedMemberId }) => {
-	const [reason, setReason] = useState("");
+const ReportModal = ({
+  onClose,
+  reportPost,
+  reportedPostId,
+  reportedMemberId,
+}) => {
+  const [reason, setReason] = useState("");
 
-	const onSubmit = () => {
-		reportPost(reason, reportedPostId, reportedMemberId);
-		onClose();
-	};
-	return (
-		<div className="modal-overlay">
-			<div className="modal-content" onClick={(e) => e.stopPropagation}>
-				<h2>신고하기</h2>
-				<textarea
-					rows="5"
-					placeholder="신고 사유를 입력해주세요..."
-					value={reason}
-					onChange={(e) => setReason(e.target.value)}
-					style={{ width: "100%", padding: "8px" }}
-				/>
-				<div className="modal-buttons">
-					<button onClick={onClose} className="close-btn">
-						취소
-					</button>
-					<button onClick={onSubmit} className="submit-btn">
-						제출
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  const onSubmit = () => {
+    reportPost(reason, reportedPostId, reportedMemberId);
+    onClose();
+  };
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content" onClick={(e) => e.stopPropagation}>
+        <h2>신고하기</h2>
+        <textarea
+          rows="5"
+          placeholder="신고 사유를 입력해주세요..."
+          value={reason}
+          onChange={(e) => setReason(e.target.value)}
+          style={{ width: "100%", padding: "8px" }}
+        />
+        <div className="modal-buttons">
+          <button onClick={onClose} className="close-btn">
+            취소
+          </button>
+          <button onClick={onSubmit} className="submit-btn">
+            제출
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const ReportButton = ({ reportedPostId, reportedMemberId, reportPost }) => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-	const openModal = () => setIsModalOpen(true);
-	const closeModal = () => setIsModalOpen(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
-	return (
-		<>
-			<Button
-				variant=""
-				className="bookmark-btn btn border rounded-circle d-flex justify-content-center align-itmes-center"
-				size="sm"
-				onClick={(e) => {
-					e.preventDefault();
-					e.stopPropagation;
-					openModal();
-				}}
-			>
-				<Image className="report-btn-img fluid" src={ReportImage} style={{ width: "17px", height: "20px" }} />
-			</Button>
-			{isModalOpen && (
-				<ReportModal
-					onClose={closeModal}
-					reportPost={reportPost}
-					reportedPostId={reportedPostId}
-					reportedMemberId={reportedMemberId}
-				/>
-			)}
-		</>
-	);
+  return (
+    <>
+      <div className="report-button-round">
+        <Button
+          variant=""
+          size="sm"
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation;
+            openModal();
+          }}
+        >
+          <Image
+            className="report-btn-img fluid"
+            src={ReportImage}
+            style={{ width: "17px", height: "20px" }}
+          />
+        </Button>
+      </div>
+      {isModalOpen && (
+        <ReportModal
+          onClose={closeModal}
+          reportPost={reportPost}
+          reportedPostId={reportedPostId}
+          reportedMemberId={reportedMemberId}
+        />
+      )}
+    </>
+  );
 };
 
 export default ReportButton;
