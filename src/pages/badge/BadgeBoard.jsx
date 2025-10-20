a003751e065e01ea145a369cd988137de43893f9
anjoo-k <knjou92@naver.com>
2025-10-20 11:41:30 +0900

    feat : subtitile 컴포넌트에 링크 props 수정 등

--- src/pages/badge/BadgeBoard.jsx
+++ src/pages/badge/BadgeBoard.jsx
@@ -2,19 +2,27 @@ import { Image } from "react-bootstrap";
 import "./BadgeBoard.css";
 import SmileFace from "../../assets/smile-face.svg";
 import Menu from "../../components/common/menu/Menu";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const BadgeAcquisition = () => {
   return (
     <>
-      <div className="justify-content-center d-flex align-items-center">
-        <div className="" xs={1}>
-          <Image src={SmileFace} />
+      <Link
+        to="/badges/map"
+        style={{
+          textDecoration: "none", // 밑줄 제거
+          color: "inherit", // 글자색 유지
+        }}
+      >
+        <div className="justify-content-center d-flex align-items-center">
+          <div className="" xs={1}>
+            <Image src={SmileFace} />
+          </div>
+          <div className="">
+            <h2 className="">사냥꾼 증표</h2>
+          </div>
         </div>
-        <div className="">
-          <h2 className="">사냥꾼 증표</h2>
-        </div>
-      </div>
+      </Link>
       <Menu
         menuOne={"문화재 인증"}
         menuOneLink={"/badges/map"}
